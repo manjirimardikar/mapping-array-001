abcea94bda8084f67c60829fb884c8164421517e
manjirimardikar <manjari.mardikar28@gmail.com>
2023-02-26 11:26:03 +0530

    first commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,42 @@
-import logo from './logo.svg';
-import './App.css';
-
+import "./App.css";
+function isPrime(number) {
+  if (number === 0 || number === 1) {
+    return false;
+  }
+  for (let i = 2; i < number; i++) {
+    if (number % i === 0) {
+      return false;
+    }
+  }
+  return true;
+}
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <div className="top">
+        <h1>30 Days Of React</h1>
+        <h2>Number Generator</h2>
+      </div>
+      <div className="middle">
+        {Array(32)
+          .fill("")
+          .map((e, i) => {
+            return (
+              <div
+                className="box"
+                style={{
+                  backgroundColor: isPrime(i)
+                    ? "#fd5e53"
+                    : i % 2 === 0
+                    ? "#21bf73"
+                    : "#fddb3a",
+                }}
+              >
+                {i}
+              </div>
+            );
+          })}
+      </div>
     </div>
   );
 }
